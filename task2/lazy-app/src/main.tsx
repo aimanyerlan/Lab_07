0bc2bd81309cda65371136fdbff732fef344bb12
aimanyerlan <yerlanaiman@mail.ru>
2026-02-27 11:27:40 +0500

    feat(lab7.2): add error boundary component

---
+++ task2/lazy-app/src/main.tsx
@@ -0,0 +1,36 @@
+import React, {Suspense, lazy} from "react";
+import ReactDOM from "react-dom/client";
+import {BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import "./index.css";
+import LoadingSpinner from "./components/LoadingSpinner";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Settings = lazy(() => import("./pages/Settings"));
+const Profile = lazy(() => import("./pages/Profile"));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav style={{ display: "flex", gap:12, padding:12 }}>
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/settings">Settings</Link>
+        <Link to="/profile">Profile</Link>
+      </nav>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
